2f93334e906cc51a8777baab103bc54850dab756
PrathamRawat <prathamrawat815@gmail.com>
2020-10-19 22:15:27 -0400

    Remove hardcoded constants for entities and replace with a list

--- src/main/scala/Requests.scala
+++ src/main/scala/Requests.scala
@@ -12,7 +12,7 @@ object Requests {
 
     }
 
-    object TezosConfigRequests {
+    object TezosConfig {
 
         final val TEZOS_NETWORKS: String = "/v2/metadata/tezos/networks"
 
@@ -20,43 +20,26 @@ object Requests {
 
     }
 
-    object TezosEntities {
+    final val TEZOS_ENTITIES: Array[String] = Array(
+        "accounts",
+        "accounts_history",
+        "baker_registry",
+        "bakers",
+        "baking_rights",
+        "balance_updates",
+        "big_map_contents",
+        "big_maps",
+        "blocks",
+        "fees",
+        "governance",
+        "known_addresses",
+        "operation_groups",
+        "operations",
+        "originated_account_maps",
+        "registered_tokens"
+    )
 
-        final val TEZOS_ACCOUNTS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/accounts/attributes"
-
-        final val TEZOS_ACCOUNTS_HISTORY_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/accounts_history/attributes"
-
-        final val TEZOS_BAKER_REGISTRY_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/baker_registry/attributes"
-
-        final val TEZOS_BAKER_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/bakers/attributes"
-
-        final val TEZOS_BAKERS_HISTORY_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/bakers_history/attributes"
-
-        final val TEZOS_BAKING_RIGHTS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/baking_rights/attributes"
-
-        final val TEZOS_BALANCE_UPDATES_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/balance_updates/attributes"
-
-        final val TEZOS_BIG_MAP_CONTENTS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/big_map_contents/attributes"
-
-        final val TEZOS_BIG_MAPS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/big_maps/attributes"
-
-        final val TEZOS_BLOCK_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/blocks/attributes"
-
-        final val TEZOS_FEES_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/fees/attributes"
-
-        final val TEZOS_GOVERNANCE_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/governance/attributes"
-
-        final val TEZOS_KNOWN_ADDRESSES_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/known_addresses/attributes"
-
-        final val TEZOS_OPERATION_GROUPS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/operation_groups/attributes"
-
-        final val TEZOS_OPERATIONS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/operations/attributes"
-
-        final val TEZOS_ORIGINATED_ACCOUNT_MAPS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/originated_account_maps/attributes"
-
-        final val TEZOS_REGISTERED_TOKENS_ATTRIBUTES: String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/registered_tokens/attributes"
-
-    }
+    def getTezosAttributeURI(entity: String): String = "/v2/metadata/tezos/" + CONSEIL_NETWORK + "/" + entity + "/attributes"
 
     object TezosChainRequests {
 
@@ -65,6 +48,8 @@ object Requests {
         final val TEZOS_GET_OPERATION_GROUP: String = "/v2/data/tezos/" + CONSEIL_NETWORK + "/operation_groups/ooJQqGxrsdxi9GNinZjivnx8y7BtMc372G8gB7GmCKSQRnNkEtK"
 
         final val TEZOS_GET_ACCOUNT: String = "/v2/data/tezos/" + CONSEIL_NETWORK + "/accounts/KT1V7VoyjbvqSmnRtv9pHkRuBCPT7UubCrCX"
+
+
 
     }
 
